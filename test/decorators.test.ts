43a4a173ddc5bca13e10f8cfa409ca9f3f56e3ed
Nico Lazarus <n.lazarus@triggerco.de>
2020-11-26 16:47:37 +0100

    🚨 improved tests

--- test/decorators.test.ts
+++ test/decorators.test.ts
@@ -11,10 +11,15 @@ const concatInputsSpy = spy();
 const hashInputsSpy = spy();
 
 class DecoratorsTest {
-  @tracked public input1: string = 'input1';
-  @tracked public input2: string = 'input2';
+  @tracked public input1: string = '';
+  @tracked public input2: string = '';
 
   @tracked public hashAlgorithm = 'md5';
+
+  constructor(input1: string, input2: string) {
+    this.input1 = input1;
+    this.input2 = input2;
+  }
 
   @cache('input1', 'input2')
   public async concatInputs() {
@@ -30,15 +35,14 @@ class DecoratorsTest {
 }
 
 describe('the \'decorators\' test', () => {
-  const decoratorsTest = new DecoratorsTest();
 
   afterEach(() => {
     concatInputsSpy.resetHistory();
   });
 
   it('should cache values if no tracked property changes and should invalidate cache if a property changes', async () => {
+    const decoratorsTest = new DecoratorsTest('input1', 'input2');
 
-    // test with default values
     const res = await decoratorsTest.concatInputs();
     assert.ok(concatInputsSpy.calledOnce);
     assert.strictEqual(res, 'input1input2');
@@ -61,28 +65,49 @@ describe('the \'decorators\' test', () => {
   });
 
   it('should invalidate cache if it depends on other cache and the other cache is invalidated', async () => {
+    const decoratorsTest = new DecoratorsTest('input1', 'input2');
 
+    // assure the concat cache is NOT hit
+    const concatVals = await decoratorsTest.concatInputs();
+    assert.ok(concatInputsSpy.calledOnce);
+    assert.strictEqual(concatVals, 'input1input2');
+
+    // assure concat cache is hit
+    // assure the hash cache NOT is hit
     const res = await decoratorsTest.hashInputs();
-    assert.ok(concatInputsSpy.notCalled);
+    assert.ok(concatInputsSpy.calledOnce);
     assert.ok(hashInputsSpy.calledOnce);
-    assert.strictEqual(res, hashString('md5', 'newinput2'));
+    assert.strictEqual(res, hashString('md5', 'input1input2'));
 
+    // assure concat cache is hit
+    // assure the hash cache is hit
     const res2 = await decoratorsTest.hashInputs();
-    assert.ok(concatInputsSpy.notCalled);
+    assert.ok(concatInputsSpy.calledOnce);
     assert.ok(hashInputsSpy.calledOnce);
-    assert.strictEqual(res2, hashString('md5', 'newinput2'));
+    assert.strictEqual(res2, hashString('md5', 'input1input2'));
 
+    // assure concat cache is hit
+    // assure chaning the hashAlgorithm is invalidating the hash cache
     decoratorsTest.hashAlgorithm = 'sha256';
     const res3 = await decoratorsTest.hashInputs();
-    assert.ok(concatInputsSpy.notCalled);
+    assert.ok(concatInputsSpy.calledOnce);
     assert.ok(hashInputsSpy.calledTwice);
-    assert.strictEqual(res3, hashString('sha256', 'newinput2'));
+    assert.strictEqual(res3, hashString('sha256', 'input1input2'));
 
-    decoratorsTest.input2 = 'new';
+    // assure concat cache is hit
+    // assure the hash cache is hit
     const res4 = await decoratorsTest.hashInputs();
     assert.ok(concatInputsSpy.calledOnce);
+    assert.ok(hashInputsSpy.calledTwice);
+    assert.strictEqual(res4, hashString('sha256', 'input1input2'));
+
+    // assure concat cache is invalidated because input1 is changed
+    // assure the hash cache is invalidated because the concat cache is invalidated
+    decoratorsTest.input1 = 'new';
+    const res5 = await decoratorsTest.hashInputs();
+    assert.ok(concatInputsSpy.calledTwice);
     assert.ok(hashInputsSpy.calledThrice);
-    assert.strictEqual(res4, hashString('sha256', 'newnew'));
+    assert.strictEqual(res5, hashString('sha256', 'newinput2'));
   });
 
   describe('error tests', () => {
