39a8329dd2e7e8117d9eed1f423654348365a05f
TBraner <t.braner@triggerco.de>
2020-11-26 15:40:10 +0100

    🔨 Refactoring

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,167 +1,191 @@
+/******************************************************************************* 
+ * Types
+*******************************************************************************/
+type TMethodName = string;
 
 type TIsDirtyMap = {
-  [cacheName: string]: boolean;
+  [methodName: string]: true;
 };
-type TPropertyCacheNameMap = {
-  [propertyName: string]: string[];
+
+type TPropertiesDependencies = {
+  [propertyName: string]: TMethodName[];
 };
 
 interface ICacheDescriptor extends TypedPropertyDescriptor<any> {
   value?: () => Promise<any>;
 }
 
-const targetCacheMap = new WeakMap<object, TIsDirtyMap>();
-const targetKeyCacheNameMap = new WeakMap<object, TPropertyCacheNameMap>();
+
+/******************************************************************************* 
+ * Variables
+*******************************************************************************/
+const objectToDirtyStates = new WeakMap<object, TIsDirtyMap>();
+const objectToDependencies = new WeakMap<object, TPropertiesDependencies>();
+
+
+/******************************************************************************* 
+ * Functions
+*******************************************************************************/
 
 /**
- * @param {object} target - class object
- * @param {string} cacheName - name of the cache to be checked if dirty
- * @return {boolean} is cache dirty
+ * Update the dirty states for the property.
  */
-function isCacheDirty(target: object, cacheName: string): boolean {
-  const isDirtyMap = targetCacheMap.get(target);
-
-  return isDirtyMap ? isDirtyMap[cacheName] : false;
+function updateDirtyStates(targetObject: object, propertyName: string) {
+  const dependencies = getDependencies(targetObject);
+  setDirtyStates(targetObject, dependencies, propertyName);
 }
 
 /**
- * @param {object} target - class object
- * @param {string} cacheName - name of the cache to be invalidated
+ * Initially builds the dependencies for the decorated targetObjects methodName.
+ *  
+ * Example for the map *dependencies* that would be created with:
+ * 
+ * @cache('firstName', 'lastName', 'data')
+ * async getFullName () { ... }
+ * 
+ * @cache('firstName')
+ * async getNameSlug () { ... }
+ * 
+ * {
+ *   firstName: [getFullName, getNameSlug],
+ *   lastName: [getFullName],
+ *   data: [getFullName],
+ *   ...: [getFullName],
+ * }
+ * 
+ * @param {...string} dependantProps - Depending properties on which the cache should be invalidated.
+ * @param {object} targetObject - class object
+ * @param {TMethodName} methodName - cache name
  */
-function invalidateCache(target: object, cacheName: string) {
-  let isDirtyMap = targetCacheMap.get(target);
+function buildDependencies(dependantProps: string[], targetObject: object, methodName: TMethodName) {
+  const dependencies = getDependencies(targetObject);
 
-  if (isDirtyMap === undefined) {
-    isDirtyMap = {
-      [cacheName]: true
-    };
-    targetCacheMap.set(target, isDirtyMap);
-  } else {
-    isDirtyMap[cacheName] = true;
-  }
-}
-
-/**
- * @param {object} target - class object
- * @param {string} cacheName - updated cache name
- */
-function cacheUpdated(target: object, cacheName: string) {
-  const isDirtyMap = targetCacheMap.get(target);
-
-  if (isDirtyMap && isDirtyMap[cacheName]) {
-    delete isDirtyMap[cacheName];
-  }
-}
-
-/**
- * @param {object} target - class object
- * @return {WeakMap<object, TPropertyCacheNameMap>}
- */
-function getCacheNameMap(target: object) {
-  let propertyMap = targetKeyCacheNameMap.get(target);
-  if (!propertyMap) {
-    propertyMap = {};
-    targetKeyCacheNameMap.set(target, propertyMap);
-  }
-
-  return propertyMap;
-}
-
-/**
- * @param {...string} dependingProperties - Depending properties on which the cache should be invalidated.
- * @param {object} target - class object
- * @param {string} cacheName - cache name
- */
-function buildPropertyCacheNameMap(dependingProperties: string[], target: object, cacheName: string) {
-  const propertyCacheMap = getCacheNameMap(target);
-
-  for (const propertyName of dependingProperties) {
-    if (!propertyCacheMap[propertyName]) {
-      propertyCacheMap[propertyName] = [cacheName];
+  for (const dependantProp of dependantProps) {
+    if (!dependencies[dependantProp]) {
+      dependencies[dependantProp] = [methodName];
     } else {
-      propertyCacheMap[propertyName].push(cacheName);
-    }
-  }
-  targetKeyCacheNameMap.set(target, propertyCacheMap);
-}
-
-/**
- * Invalidate all caches that are depending on this property. This function is recrucive.
- *
- * @param {object} target - class object
- * @param {TPropertyCacheNameMap} propertyCacheMap - class object
- * @param {string} propertyName - name of the dirty object property
- */
-function invalidateCaches(target: object, propertyCacheMap: TPropertyCacheNameMap, propertyName: string) {
-  const cachePropertyNames = propertyCacheMap[propertyName];
-
-  if (cachePropertyNames) {
-    for (const cachePropertyName of cachePropertyNames) {
-      invalidateCache(target, cachePropertyName);
-      invalidateCaches(target, propertyCacheMap, cachePropertyName);
+      dependencies[dependantProp].push(methodName);
     }
   }
 }
 
 /**
- * Invalidate all caches that are depending on this property
- *
- * @param {object} target - class object
- * @param {string} propertyName - name of the updated object property
+ * Checks if a methodName is in the map that contains the state about all
+ * dirty methods.
  */
-function propertyUpdated(target: object, propertyName: string) {
-  const propertyCacheMap = getCacheNameMap(target);
-  invalidateCaches(target, propertyCacheMap, propertyName);
+function isCacheDirty(targetObject: object, methodName: TMethodName): boolean {
+  const dirtyStates = objectToDirtyStates.get(targetObject);
+  return !!dirtyStates && methodName in dirtyStates;
 }
+
+/**
+ * Removes a methodName from the map that contains the dirty states
+ */
+function unsetIsCacheDirty(targetObject: object, methodName: TMethodName) {
+  const dirtyStates = objectToDirtyStates.get(targetObject);
+
+  if (dirtyStates && dirtyStates[methodName]) {
+    delete dirtyStates[methodName];
+  }
+}
+
+/**
+ * Fetches the dependencies for all properties and methods of one targetObject
+ */
+function getDependencies(targetObject: object) {
+  let propertiesDependencies = objectToDependencies.get(targetObject);
+  // if empty -> initialize map with empty object
+  if (!propertiesDependencies) {
+    propertiesDependencies = {};
+    objectToDependencies.set(targetObject, propertiesDependencies);
+  }
+
+  return propertiesDependencies;
+}
+
+/**
+ * Adds the dirty flag to the states map for the property and all of its
+ * dependencies recursively.
+ */
+function setDirtyStates(targetObject: object, dependencies: TPropertiesDependencies, propOrMethod: string | TMethodName) {
+  const methodNames = dependencies[propOrMethod];
+
+  if (methodNames) {
+    for (const methodName of methodNames) {
+      setDirtyState(targetObject, methodName);
+      setDirtyStates(targetObject, dependencies, methodName);
+    }
+  }
+}
+
+/**
+ * Invalidates the cache by adding a true value for the methodName in the
+ * dirty states map.
+ */
+function setDirtyState(targetObject: object, methodName: TMethodName) {
+  let dirtyStates = objectToDirtyStates.get(targetObject);
+
+  if (dirtyStates === undefined) {
+    dirtyStates = {
+      [methodName]: true
+    };
+    objectToDirtyStates.set(targetObject, dirtyStates);
+  } else {
+    dirtyStates[methodName] = true;
+  }
+}
+
+/******************************************************************************* 
+ * Decorators
+*******************************************************************************/
+
+/**
+ * Tracked changes of a property to reference them in the cache method decorator
+ */
+export function tracked(targetObject: object, propertyName: string) {
+  let value: any;
+  Object.defineProperty(targetObject, propertyName, {
+    configurable: true, // property can change the type
+    get() {
+      return value;
+    },
+    set(newValue) {
+      if (targetObject[propertyName] !== newValue) {
+        updateDirtyStates(targetObject, propertyName);
+      }
+      value = newValue;
+    }
+  });
+}
+
 
 /**
  * Method decorator to cache the return value of the method. The method called only at first time and if one of depending properties changes.
  *
- * @param {...string} dependingProperties - Depending properties on which the cache should be invalidated.
+ * @param {...string} dependantProperties - Depending properties on which the cache should be invalidated.
  */
-export function cache(...dependingProperties: string[]) {
+export function cache(...dependantProperties: string[]) {
   let cacheValue: any;
 
   return function (
-    target: object,
-    key: string,
+    targetObject: object,
+    methodName: TMethodName,
     descriptor: ICacheDescriptor
   ) {
     if (!descriptor?.value || typeof descriptor.value !== 'function') {
       throw Error('No valid property descriptor value. Property descriptor value should be \'() => Promise<any>\'');
     }
 
-    buildPropertyCacheNameMap(dependingProperties, target, key);
-    const originalMethod = descriptor.value.bind(target);
+    buildDependencies(dependantProperties, targetObject, methodName);
+    const originalMethod = descriptor.value.bind(targetObject);
 
     descriptor.value = async () => {
-      if (!cacheValue || isCacheDirty(target, key)) {
+      if (!cacheValue || isCacheDirty(targetObject, methodName)) {
         cacheValue = await originalMethod();
-        cacheUpdated(target, key);
+        unsetIsCacheDirty(targetObject, methodName);
       }
 
       return cacheValue;
     };
   };
-}
-
-/**
- * Tracked changes of a property to reference them in the cache method decorator
- *
- */
-export function tracked(target: object, key: string) {
-  let value: any;
-  Object.defineProperty(target, key, {
-    configurable: true,
-
-    get() {
-      return value;
-    },
-    set(newValue) {
-      if (target[key] !== newValue) {
-        propertyUpdated(target, key);
-      }
-      value = newValue;
-    }
-  });
 }
